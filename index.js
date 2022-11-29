27dff44c8b08a6db7ba0fc229d8009d87118af19
lazypool <xiao_xiao_pku@163.com>
2022-11-29 20:11:02 +0800

    add route for login/register

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/' + 'index.html')
 })
 
+app.get('login', function(req, res) {
+	res.sendFile(__dirname + '/html' + '/login.html')
+}
+
+app.get('register', function(req, res) {
+	res.sendFile(__dirname + '/html' + '/register.html')
+})
+)
+
 app.listen(8080, function() {
 	console.log('server running at http://127.0.0.1:8080')
 })
