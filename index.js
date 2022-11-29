ecb362fbfb7cae6a7c8f5763cd3b79ed75167207
lazypool <xiao_xiao_pku@163.com>
2022-11-29 20:32:56 +0800

    add comments

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const app = express()
 
+// 提供静态资源
 app.use('/', express.static('public'))
 
+// 访问首页
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/' + 'index.html')
 })
 
+// 访问登陆界面
 app.get('/login', function(req, res) {
 	res.sendFile(__dirname + '/html' + '/login.html')
 })
 
+// 访问注册界面
 app.get('/register', function(req, res) {
 	res.sendFile(__dirname + '/html' + '/register.html')
 })
 
+// 监听8080端口
 app.listen(8080, function() {
 	console.log('server running at http://127.0.0.1:8080')
 })
