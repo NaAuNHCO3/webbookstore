72f4af746d7721a6a958714ef26ca7ab142eaae6
NaAuNHCO3 <1522096134@qq.com>
2022-11-29 22:57:18 +0800

    add router

--- index.js
+++ index.js
@@ -4,20 +4,9 @@ const app = express()
 // 提供静态资源
 app.use('/', express.static('public'))
 
-// 访问首页
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/' + 'index.html')
-})
-
-// 访问登陆界面
-app.get('/login', function(req, res) {
-	res.sendFile(__dirname + '/html' + '/login.html')
-})
-
-// 访问注册界面
-app.get('/register', function(req, res) {
-	res.sendFile(__dirname + '/html' + '/register.html')
-})
+// 建立路由
+const base = require('./public/lib/base.js')
+app.use('/', base)
 
 // 监听8080端口
 app.listen(8080, function() {
