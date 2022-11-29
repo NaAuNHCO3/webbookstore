41d41ba4c0ae4217328244945074899bad076534
lazypool <xiao_xiao_pku@163.com>
2022-11-29 19:58:09 +0800

    add route for the index.html

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express()
 
 app.use('/', express.static('public'))
 
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/' + 'index.html')
+})
+
 app.listen(8080, function() {
 	console.log('server running at http://127.0.0.1:8080')
 })
