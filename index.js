47f4b268fb6a7b7733ff168bde9c7edd1b88bd9e
Sil1cateS <1334676300@qq.com>
2022-12-01 21:53:12 +0800

    Front-end ver0.2 update

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express()
 // 提供静态资源
 app.use('/', express.static('public'))
 
-// 建立路由
+app.use(express.urlencoded({ extended:false }))
+
+// base路由，访问首页、登陆界面、注册界面
 const base = require('./public/lib/base.js')
 app.use('/', base)
+
+// profile路由，访问用户界面，更改个人信息等
+const profile = require('./public/lib/profile.js')
+app.use('/', profile)
 
 // 监听8080端口
 app.listen(8080, function() {
