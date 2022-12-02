a0df0d2acf18d8931df894ff75c4ac2ed445b4c4
lazypool <xiao_xiao_pku@163.com>
2022-12-02 12:36:11 +0800

    add exec.js

---
+++ public/lib/exec.js
@@ -0,0 +1,43 @@
+const db = require('./sql.js')
+
+const exec = function(sqlArr) {
+	return new Promise((resolve, reject) {
+		const promiseArr = []
+		db.getConnection(function(err, conn) {
+			if(err) {
+				return reject(err)
+			}
+			conn.beginTransaction(function(err) {
+				if(err) {
+					return reject(err)
+				}
+				// 将所有需要执行的sql封装为数组
+				promiseArr = sqlArr.map(function({sql, value}) {
+					return new Promise(function(resolve, reject) {
+						conn.query(sql, value, function(e, results) {
+							e ? reject(e) : resolve({ rows, success: true })
+						})
+					})
+				})
+				// Promise调用所有的sql，一旦出错则回滚，否则提交事务并释放连接
+				Promise.all(promiseArr).then(function(res) {
+					conn.commit(function(err) {
+						if(err) {
+							console.log('commit failed')
+							reject(err)
+						}
+						conn.release()
+						resolve(res)
+					}).catch(function(err) {
+						conn.rollback(function() {
+							console.log('rollback')
+						})
+						reject(err)
+					})
+				})
+			})
+		})
+	})
+}
+
+module.exports = exec
