40a8eb38cf58df2262be4aa3fd2c82124c31bce6
lazypool <xiao_xiao_pku@163.com>
2022-12-08 15:44:49 +0800

    implutent the method of the bookinfo

--- public/js/bookinfo.js
+++ public/js/bookinfo.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
 		{ bookid: window.sessionStorage.getItem("bookid") },
 		function(message) {
 			if(message.status == 0){
-				alert(message.msg)
+				var data = message.data
+				$("#bookname").text(data.bookname)
+				$("#author").text(data.author)
+				$("#press").text(data.press)
+				$("#bookprice").text("￥"+data.bookprice)
+				$("#bookinvent").text(data.bookinvent+"本")
+				$("#bookabstract").text(data.bookabstract)
 			} else {
 				alert(message.msg)
 			}
