772850320271b0e31fc08fe16502452dace1ed3b
NaAuNHCO3 <1522096134@qq.com>
2022-12-12 02:04:43 +0800

    makeorder

--- public/lib/cart.js
+++ public/lib/cart.js
@@ -10,4 +10,18 @@ router.get('/cart',function(req, res){
     res.sendFile('html/cart.html', { root , root })
 })
 
+// 下单
+router.post('/makeorder', function(req, res){
+    const body=req.body
+    db.getConnection(function(err, conn){
+        if(err){
+            console.log(err.message)
+            return res.send({
+                status:1,
+                msg:'connect failed'
+            })
+        }
+        var sql = 'insert into orders '
+    })
+})
 module.exports = router
